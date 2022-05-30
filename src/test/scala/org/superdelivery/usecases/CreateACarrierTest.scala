55465607af9a0828e9e9b2f1a9e26d2b33c7b6c0
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2022-05-30 15:04:09 +0200

    refactor(carrier-creation): Extract test data

--- src/test/scala/org/superdelivery/usecases/CreateACarrierTest.scala
+++ src/test/scala/org/superdelivery/usecases/CreateACarrierTest.scala
@@ -1,7 +1,8 @@
 package org.superdelivery.usecases
 
 import munit.FunSuite
-import org.superdelivery.Commands.CreateCarrierCommand
+import org.superdelivery.Data
+import org.superdelivery.Data.command
 import org.superdelivery.model.{Area, Carrier, CarrierId, Point, Timeslot}
 
 import java.time.LocalTime
@@ -10,19 +11,7 @@ class CreateACarrierTest extends FunSuite {
   private val repository = new InMemoryDB[CarrierId, Carrier](_.carrierId)
   private val sut = new CreateACarrier(repository)
   test("return a Carrier with a random ID and same properties") {
-    val result = sut.handle(
-      CreateCarrierCommand(
-        name = "John express",
-        workingRange =
-          Timeslot(LocalTime.parse("09:00"), LocalTime.parse("18:00")),
-        workingArea = Area(Point(43.2969901, 5.3789783), 42),
-        maxWeight = 50,
-        maxVolume = 40,
-        maxPacketWeight = 30,
-        speed = 20,
-        cost = 10
-      )
-    )
+    val result = sut.handle(command)
 
     val expected = Carrier(
       carrierId = CarrierId("john-express"),
@@ -40,19 +29,7 @@ class CreateACarrierTest extends FunSuite {
   }
 
   test("save created carrier in DB") {
-    sut.handle(
-      CreateCarrierCommand(
-        name = "John express",
-        workingRange =
-          Timeslot(LocalTime.parse("09:00"), LocalTime.parse("18:00")),
-        workingArea = Area(Point(43.2969901, 5.3789783), 42),
-        maxWeight = 50,
-        maxVolume = 40,
-        maxPacketWeight = 30,
-        speed = 20,
-        cost = 10
-      )
-    )
+    sut.handle(command)
 
     val expected = Some(
       Carrier(
@@ -71,26 +48,17 @@ class CreateACarrierTest extends FunSuite {
     assertEquals(repository.get(CarrierId("john-express")), expected)
   }
 
-    test("reject if a carrier with same ID exists") {
-      val command = CreateCarrierCommand(
-        name = "John express",
-        workingRange =
-          Timeslot(LocalTime.parse("09:00"), LocalTime.parse("18:00")),
-        workingArea = Area(Point(43.2969901, 5.3789783), 42),
-        maxWeight = 50,
-        maxVolume = 40,
-        maxPacketWeight = 30,
-        speed = 20,
-        cost = 10
-      )
-      sut.handle(command)
+  test("reject if a carrier with same ID exists") {
+    sut.handle(Data.command)
 
-      val result = sut.handle(command.copy(
+    val result = sut.handle(
+      Data.command.copy(
         maxVolume = 100
-      ))
+      )
+    )
 
     val expected = Left("A carrier with same ID already exists")
-      assertEquals(result, expected)
+    assertEquals(result, expected)
   }
 
 }
